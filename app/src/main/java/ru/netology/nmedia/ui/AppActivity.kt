1894cfc4fa11a5dc94b89af9f3a07e9d75c2fcb6
Alex Sergeev <sanhokk@yandex.ru>
2024-06-27 22:31:53 +0300

    make animation

--- app/src/main/java/ru/netology/nmedia/ui/AppActivity.kt
+++ app/src/main/java/ru/netology/nmedia/ui/AppActivity.kt
@@ -1,6 +1,7 @@
 package ru.netology.nmedia.ui
 
 import android.os.Bundle
+import android.view.animation.AnimationUtils
 import androidx.appcompat.app.AppCompatActivity
 import ru.netology.nmedia.R
 
@@ -8,11 +9,17 @@ class AppActivity : AppCompatActivity(R.layout.activity_app) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        findViewById<StatsView>(R.id.stats).data = listOf(
-            500F,
-            500F,
-            500F,
-            500F
-        )
+        val view = findViewById<StatsView>(R.id.stats)
+
+        view.postDelayed({
+            view.data = listOf(
+                0.25F,
+                0.25F,
+                0.25F,
+                0.25F,
+            )
+        }, 100)
+
+        view.startAnimation(AnimationUtils.loadAnimation(this, R.anim.animation))
     }
 }
--- app/src/main/java/ru/netology/nmedia/ui/StatsView.kt
+++ app/src/main/java/ru/netology/nmedia/ui/StatsView.kt
@@ -1,5 +1,6 @@
 package ru.netology.nmedia.ui
 
+import android.animation.ValueAnimator
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Paint
@@ -7,6 +8,7 @@ import android.graphics.PointF
 import android.graphics.RectF
 import android.util.AttributeSet
 import android.view.View
+import android.view.animation.LinearInterpolator
 import androidx.core.content.withStyledAttributes
 import ru.netology.nmedia.R
 import ru.netology.nmedia.util.AndroidUtils
@@ -25,11 +27,10 @@ class StatsView @JvmOverloads constructor(
     defStyleAttr,
     defStyleRes,
 ) {
-
     var data: List<Float> = emptyList()
         set(value) {
             field = value
-            invalidate()
+            update()
         }
 
     private var radius = 0F
@@ -38,8 +39,10 @@ class StatsView @JvmOverloads constructor(
     private var textSize = AndroidUtils.dp(context, 40F).toFloat()
     private var lineWidth = AndroidUtils.dp(context, 16F).toFloat()
     private var colors = emptyList<Int>()
-    private fun smartCounter(sum: Float): Float = if(sum>1) sum.pow(-1) else 1F
+    private fun smartCounter(sum: Float): Float = if (sum > 1) sum.pow(-1) else 1F
 
+    private var progress = 0F
+    private var valueAnimator: ValueAnimator? = null
 
     private val paint = Paint(
         Paint.ANTI_ALIAS_FLAG
@@ -67,6 +70,25 @@ class StatsView @JvmOverloads constructor(
         }
     }
 
+    private fun update() {
+        valueAnimator?.let {
+            it.removeAllListeners()
+            it.cancel()
+        }
+        progress = 0F
+
+        valueAnimator = ValueAnimator.ofFloat(0F, 1F).apply {
+            addUpdateListener { anim ->
+                progress = anim.animatedValue as Float
+                invalidate()
+            }
+            duration = 3000
+            interpolator = LinearInterpolator()
+        }.also {
+            it.start()
+        }
+    }
+
     private fun randomColor() = Random.nextInt(0xFF000000.toInt(), 0xFFFFFFFF.toInt())
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
@@ -86,16 +108,16 @@ class StatsView @JvmOverloads constructor(
         }
 
         var startFrom = -90F
-        for ((index, datum) in data.withIndex()) {
+        data.forEachIndexed { index, datum ->
             val angle = 360F * datum * smartCounter(data.sum())
             paint.color = colors.getOrNull(index) ?: randomColor()
-            canvas.drawArc(oval, startFrom, angle, false, paint)
+            canvas.drawArc(oval, startFrom, angle * progress, false, paint)
             startFrom += angle
         }
         paint.color = colors[0]
 
         canvas.drawText(
-            "%.2f%%".format(data.sum() * 100 * smartCounter(data.sum())),
+            "%.2f%%".format(data.sum() * progress * 100 * smartCounter(data.sum())),
             center.x,
             center.y + textPaint.textSize / 4,
             textPaint,
